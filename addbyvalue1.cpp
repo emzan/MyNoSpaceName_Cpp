56ec3ca5ea8158c25e48b3156d26976bb486a070
emzan <zanettiemilio@hotmail.com>
2018-06-02 18:49:16 +0800

    okCommettoerrorevolmente

--- addbyvalue1.cpp
+++ addbyvalue1.cpp
@@ -1,8 +1,20 @@
-//
-//  addbyvalue1.cpp
-//  
-//
-//  Created by George Martinson on 2018/6/2.
-//
 
-#include "addbyvalue1.hpp"
+#include <iostream>
+
+void addfive(int a, int b)
+{
+    std::cout << "entering addfive function" << std::endl;
+    std::cout << "a and b before adding 5: a=" << a << " b=" << b << std::endl << std::endl;
+    a += 5;
+    b += 5;
+    std::cout << "a and b after adding 5: a=" << a << " b=" << b << std::endl;
+    std::cout << "leaving addfive function" << std::endl << std::endl;
+}
+int main()
+{
+    int a = 10, b = 15;
+    std::cout << "a and b before calling addfive function: a=" << a << " b=" << b << std::endl;
+    addfive(a, b);
+    std::cout << "a and b after calling addfive function: a=" << a << " b=" << b << std::endl;
+    std::cin.get();
+}
