fcdf20c442f78bc51f537d7628bc066349041bd4
emzan <zanettiemilio@hotmail.com>
2018-06-02 18:47:44 +0800

    ok

---
+++ addbyvalue1.cpp
@@ -0,0 +1,8 @@
+//
+//  addbyvalue1.cpp
+//  
+//
+//  Created by George Martinson on 2018/6/2.
+//
+
+#include "addbyvalue1.hpp"
---
+++ addbyvalue1.hpp
@@ -0,0 +1,13 @@
+//
+//  addbyvalue1.hpp
+//  
+//
+//  Created by George Martinson on 2018/6/2.
+//
+
+#ifndef addbyvalue1_hpp
+#define addbyvalue1_hpp
+
+#include <stdio.h>
+
+#endif /* addbyvalue1_hpp */
